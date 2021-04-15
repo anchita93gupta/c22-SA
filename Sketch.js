75f0fc13dd01b95f1bcd294ad2583c74512be464
anchita93gupta <76733471+anchita93gupta@users.noreply.github.com>
2021-04-15 11:30:52 +0530

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -1,1 +1,27 @@
+const World=Matter.World;
+const Engine=Matter.Engine;
+const Bodies=Matter.Bodies;
+
+var engine;
+
+function setup(){
+  var canvas=createCanvas(400,400);
+  engine = Engine.create();
+    world = engine.world;
+    var object_options={
+      isStatic: true
+    }
+    object = Bodies.rectangle(200,100,20,20,object_options);
+    World.add(world,object);
+
+  console.log(object);
+}
+function draw()
+{
+  background(0);
+  Engine.update(engine);
+  rect(object.position.x,object.position.y,50,50);
+
+  
+}
 
